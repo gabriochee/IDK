648d9c747f342cc5a34f9e6c3ab2bf09db082af8
gabriochee <gabypla2005@gmail.com>
2024-03-16 23:04:15 +0100

    Made first version of back office's home.

--- admin/home_backoffice.php
+++ admin/home_backoffice.php
@@ -11,8 +11,26 @@
 <body>
     <?php require('../inc/header_backoffice.php');?>
     <main>
-
+        <div class="container-fluid d-flex justify-content-center my-5 py-2">
+            <img src="../inc/logo.svg" alt="Logo IDK" class="img-thumbnail bg-transparent border-0">
+        </div>
+        <ul class="fs-5 fw-bold">
+            <li class="mt-2">Nombres d'utilisateurs connecté(s) : 1</li>
+            <li class="mt-2">Nombres de profils administrateurs : 1</li>
+            <li class="mt-2">Nombres d'utilisateurs inscrits : 1(24h) / 1(7j) / 1(30j) / 1(1a) / 1(all)</li>
+            <li class="mt-2">Nombres d'utilisations du questionnaire : 1(24h) / 1(7j) / 1(30j) / 1(1a) / 1(all)</li>
+            <li class="mt-2">Nombres d'utilisations de la fusion : 1(24h) / 1(7j) / 1(30j) / 1(1a) / 1(all)</li>
+            <li class="mt-2">Nombres d'utilisations de l'anti-film : 1(24h) / 1(7j) / 1(30j) / 1(1a) / 1(all)</li>
+            <li class="mt-2">Genre le plus conseillé pour films : horreur</li>
+            <li class="mt-2">Genre les plus conseillé pour séries : horreur</li>
+            <li class="mt-2">Moyenne d’age des utilisateurs inscrits : 1an</li>
+            <li class="mt-2">Moyenne de listes par utilisateurs (2 par défaut à l’inscription) : 1</li>
+            <li class="mt-2">Nombres d’oeuvre dans la base : 1</li>
+            <li class="mt-2">Nombres de films dans la base : 1</li>
+            <li class="mt-2">Nombres de séries dans la base : 1</li>
+        </ul>
     </main>
+
     <?php require('../inc/footer_backoffice.php'); ?>
     <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
