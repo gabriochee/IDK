3f26f187d0f0b7ff0c789b6f43920ef180d4e0e6
gabriochee <gabypla2005@gmail.com>
2024-03-16 11:22:45 +0100

    Coded header for non connected users.

---
+++ client/home_not_connected.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../inc/style.css">
+    <title>IDK</title>
+</head>
+<body>
+    <?php require('../inc/header_not_connected.php');?>
+    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
