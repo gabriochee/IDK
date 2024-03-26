10bf339ef942edd25247f0785bbbf0bfc14bba6b
gabriochee <gabypla2005@gmail.com>
2024-03-26 18:18:11 +0100

    Added terms and conditions pages.

--- client/connected/about.php
+++ client/connected/about.php
@@ -17,7 +17,7 @@
     <main>
         <div class="container-fluid d-flex justify-content-center">
             <div class="d-flex flex-column my-5 py-2 gap-5 text-center">
-                <img src="../../inc/logo.svg" alt="Logo IDK" width="200px" height="200px" class="img-thumbnail bg-transparent border-0">
+                <img src="../../inc/logo.svg" alt="Logo IDK" width="200px" height="200px" class="container-fluid img-thumbnail bg-transparent border-0">
                 <h3 class="mt-5">A propos</h3>
             </div>
         </div>
