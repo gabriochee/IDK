857aec618a7acd05fe2d6da659f9eb8fb61c7673
gabriochee <gabypla2005@gmail.com>
2024-04-03 20:54:35 +0200

    questionnaire for connected.

---
+++ client/connected/questionnaire_results.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../inc/style.css">
+    <link rel="stylesheet" href="../../bootstrap/bootstrap-icons/font/bootstrap-icons.min.css">
+    <script src="../../bootstrap/js/bootstrap.bundle.min.js"></script>
+    <title>IDK</title>
+</head>
+
+<body class="overflox-y-hidden">
+    <?php require('../../inc/not_connected/header.php'); ?>
+
+    <main>
+        <div class="container-fluid d-flex flex-column text-center">
+            <div class="my-5 py-2 gap-5">
+                <img src="../../inc/logo.svg" alt="Logo IDK" width="200px" height="200px" class="img img-thumbnail bg-transparent border-0">
+            </div>
+            <h3 class="mt-5 mx-auto col-6">Voici selon notre algorithme, les cinqs œuvres qui correspond le plus a vos réponses.
+                <br>Bon visionnage !
+            </h3>
+        </div>
+
+        <div class="d-lg-flex row row-cols-lg-6 row-cols-2 row-cols-md-3 gap-3 justify-content-center mt-5 pt-5">
+            <div class="card border-2 rounded-0" style="background-color: #CFDBD5;">
+                <img src="../../inc/movie_card_placeholder.jpg" class="card-img-top rounded-0 border-2 border-dark p-4" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title mb-4">Nom oeuvre</h5>
+                    <a href="#" class="nav-btn btn btn-primary btn-sm btn-warning border border-dark border-2 rounded-3 fs-4">En voir plus</a>
+                </div>
+            </div>
+
+            <div class="card border-2 rounded-0" style="background-color: #CFDBD5;">
+                <img src="../../inc/movie_card_placeholder.jpg" class="card-img-top rounded-0 border-2 border-dark p-4" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title mb-4">Nom oeuvre</h5>
+                    <a href="#" class="nav-btn btn btn-primary btn-sm btn-warning border border-dark border-2 rounded-3 fs-4">En voir plus</a>
+                </div>
+            </div>
+
+            <div class="card border-2 rounded-0" style="background-color: #CFDBD5;">
+                <img src="../../inc/movie_card_placeholder.jpg" class="card-img-top rounded-0 border-2 border-dark p-4" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title mb-4">Nom oeuvre</h5>
+                    <a href="#" class="nav-btn btn btn-primary btn-sm btn-warning border border-dark border-2 rounded-3 fs-4">En voir plus</a>
+                </div>
+            </div>
+
+            <div class="card border-2 rounded-0" style="background-color: #CFDBD5;">
+                <img src="../../inc/movie_card_placeholder.jpg" class="card-img-top rounded-0 border-2 border-dark p-4" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title mb-4">Nom oeuvre</h5>
+                    <a href="#" class="nav-btn btn btn-primary btn-sm btn-warning border border-dark border-2 rounded-3 fs-4">En voir plus</a>
+                </div>
+            </div>
+
+            <div class="card border-2 rounded-0" style="background-color: #CFDBD5;">
+                <img src="../../inc/movie_card_placeholder.jpg" class="card-img-top rounded-0 border-2 border-dark p-4" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title mb-4">Nom oeuvre</h5>
+                    <a href="#" class="nav-btn btn btn-primary btn-sm btn-warning border border-dark border-2 rounded-3 fs-4">En voir plus</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="container-fluid d-flex flex-column text-center">
+            <h3 class="mt-5 mx-auto col-6 fw-normal">Merci pour votre participation.<br>Pour plus d’options : Inscrivez-vous !</h3>
+        </div>
+
+        <div class="container-fluid text-center mt-5">
+            <a href="#" class="btn btn-primary btn-sm btn-warning border border-dark border-2 rounded-3 fs-4 col-md-3">Fusionner</a>
+        </div>
+
+        <div class="container-fluid text-center mb-5 mt-2">
+            <a href="#" class="btn btn-primary btn-sm btn-warning border border-dark border-2 rounded-3 fs-4 col-md-3">Ajouter à une liste</a>
+        </div>
+
+        <div class="container-fluid col-10 fs-5 border border-2 border-dark overflow-auto max-height" style="background-color : #CFDBD5;">
+            <ul>
+                <li class="py-2">Liste 1 - Ajouter</li>
+                <li class="py-2"> Liste 2 - Ajouter</li>
+                <li class="py-2"> Liste 3 - Ajouter</li>
+                <li class="py-2"> Liste 4 - Ajouter</li>
+                <li class="py-2"> Liste 5 - Ajouter</li>
+                <li class="py-2"> Liste 6 - Ajouter</li>
+                <li class="py-2"> Liste 7 - Ajouter</li>
+                <li class="py-2"> Liste 8 - Ajouter</li>
+                <li class="py-2"> Liste 9 - Ajouter</li>
+                <li class="py-2"> Liste 10 - Ajouter</li>
+                <li class="py-2"> Liste 11 - Ajouter</li>
+            </ul>
+        </div>
+
+    </main>
+
+    <?php require('../../inc/not_connected/footer.php'); ?>
+</body>
+
+</html>
