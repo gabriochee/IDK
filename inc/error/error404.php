1855dfc8f626d5beed929cefc32fe833e9e63c26
gabriochee <gabypla2005@gmail.com>
2024-04-11 10:46:15 +0200

    Finished first version of error page.

--- inc/error/error404.php
+++ inc/error/error404.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../inc/functions.php">
-    <link rel="stylesheet" href="../inc/style.css">
-    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../bootstrap/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../bootstrap/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../style.css">
     <title>IDK</title>
 </head>
-<!-- <body> faire un renvoie vers connected/home ou not-connected/home en fonction du statue  -->
-    <main class="container-fluid d-flex no-wrap flex-column">
+<!--  faire un renvoie vers connected/home ou not-connected/home en fonction du statue  -->
+
+<body class="container-fluid min-vh-100 error-background d-flex flex-column text-center align-items-center">
+    <!-- <main class="container-fluid d-flex no-wrap flex-column">
         <div class="d-flex justify-content-between">
             <img src="../logo.svg" class="img img-fluid" width="120px" alt="">
             <img src="../logo.svg" class="img img-fluid" width="120px" alt="">
@@ -26,9 +28,17 @@
             <img src="../logo.svg" class="img img-fluid" width="120px" alt="">
             <img src="../logo.svg" class="img img-fluid" width="120px" alt="">
         </div>
-    </main>
+    </main> -->
 
-<script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="../inc/script.js"></script>
+    <div class="row flex-grow-1 align-items-center">
+        <div class="container bg-idk p-5">
+            <h1 class="display-1 fw-bold">Erreur</h1>
+            <h1 class="display-1 fw-bold">404</h1>
+        </div>
+    </div>
+
+    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../inc/script.js"></script>
 </body>
+
 </html>
