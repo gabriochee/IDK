4046baf9148f87bb81796bb5c326cfbe8a435a79
gabriochee <gabypla2005@gmail.com>
2024-06-04 16:49:50 +0200

    Modified oeuvre search parameters and display.

--- inc/php/function_oeuvre.php
+++ inc/php/function_oeuvre.php
@@ -2,7 +2,7 @@
 if(isset($_GET['mv'])) {
     $req1 = $bdd->query("SELECT primaryTitle, startYear, runtimeMinutes FROM work_basics WHERE id_work = {$_GET['mv']}");
     $rep1 = $req1->fetch();
-    //$req2 = $bdd->query("SELECT region, language FROM work_akas WHERE id_work = {$_GET['mv']} AND ordering ='1'");
+    //$req2 = $bdd->query("SELECT region, language FROM work_akas WHERE id_work = {$_GET['mv']};");
     //$rep2 = $req2->fetch();
     $req3 = $bdd->query("SELECT genre FROM work_genres WHERE id_work = {$_GET['mv']}");
     $rep3 = $req3->fetchAll(); 
