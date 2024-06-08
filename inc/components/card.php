d127bedac46093516bed73ce03d386bb27881cd7
gabriochee <gabypla2005@gmail.com>
2024-06-08 14:11:55 +0200

    New list page style

--- inc/components/card.php
+++ inc/components/card.php
@@ -1,5 +1,5 @@
 <div class="card border-2 rounded-0 col" style="background-color: #CFDBD5;">
-    <img src="../../inc/movie_card_placeholder.jpg" class="card-img-top rounded-0 border-2 border-dark mt-1 p-xl-4 p-md-2 p-sm-4" alt="...">
+    <img src="../../inc/img/film.svg" class="card-img-top rounded-0 border-2 border-dark mt-1 p-xl-4 p-md-2 p-sm-4" alt="...">
     <div class="card-body text-center">
         <h5 class="card-title mb-4">Nom oeuvre</h5>
         <a href="#" class="nav-btn btn btn-primary btn-sm btn-warning border border-dark border-2 rounded-3 fs-4">En voir plus</a>
