7c9078367532201f00029d8e198654470dde4d6b
sangeric <eric.sang.pro@gmail.com>
2024-06-12 21:47:11 +0200

    connexion

--- inc/php/function_connexion.php
+++ inc/php/function_connexion.php
@@ -9,11 +9,10 @@ require '../../inc/library/PHPMailer/src/Exception.php';
 require '../../inc/library/PHPMailer/src/PHPMailer.php';
 require '../../inc/library/PHPMailer/src/SMTP.php';
 
+
 $email = $_SESSION['email'];
 
-if(isset($_POST['code'])) {
-
-
+if (isset($_POST['code'])) {
     $mail = new PHPMailer(true);
     $mail->isSMTP();
     
@@ -25,43 +24,70 @@ if(isset($_POST['code'])) {
     $mail->Port = 465;
     
     $mail->setFrom('annuelprojet2@gmail.com');
-    $email = $_SESSION['email'];
     $mail->addAddress($email);
     $mail->isHTML(true);
 
     $verification_code = substr(number_format(time() * rand(), 0, '', ''), 0, 6);
     $mail->Subject = 'Email verification';
-    $mail->Body    = '<p>Your verification code is: <b style="font-size: 30px;">' . $verification_code . '</b></p>';
+    $mail->Body = '<p>Your verification code is: <b style="font-size: 30px;">' . $verification_code . '</b></p>';
 
-    
-    $req = $bdd->prepare("UPDATE utilisateur SET verification_code = :verification_code WHERE mail = :email;");
-    $req->execute( array("email" => $email, "verification_code" => $verification_code) );
-    
     try {
-        $mail->send();
-        header('Location: confirmation_connexion.php?email_sent=true');
-        exit; 
+        $req = $bdd->prepare("UPDATE utilisateur SET verification_code = :verification_code WHERE mail = :email;");
+        $req->bindValue(':email', $email, PDO::PARAM_STR);
+        $req->bindValue(':verification_code', $verification_code, PDO::PARAM_STR);
+        $req->execute();
+        //vérif si la requete a fonctionné vérifie si la requete a modifié une ligne dans la bdd ou pas ?
+        if ($req->rowCount() > 0) {
+            if ($mail->send()) {
+                header('Location: confirmation_connexion.php?email_sent=true');
+                exit;
+            } else {
+                header('Location: confirmation_connexion.php?mail_error=true&error=' . urlencode($mail->ErrorInfo));
+                exit;
+            }
+        } else {
+            header('Location: confirmation_connexion.php?update_failed=true');
+            exit;
+        }
     } catch (PDOException $e) {
-        echo "Erreur : " . $e->getMessage();
-        echo "Code erreur : " . $e->getCode();
-        echo "Erreur lors de l'envoi de l'e-mail : " . $mail->ErrorInfo;
+        header('Location: confirmation_connexion.php?db_error=true&error=' . urlencode($e->getMessage()));
+        exit;
+    } catch (Exception $e) {
+        header('Location: confirmation_connexion.php?mail_error=true&error=' . urlencode($e->getMessage()));
+        exit;
     }
 }
 
-if(isset($_POST['connect'])) {
+if (isset($_POST['connect'])) {
     $connect = $_POST['entrer_code'];
-    if($connect != "") {
+    if ($connect != "") {
         $email = $_SESSION['email'];
-        $req = $bdd->prepare("SELECT mail, verification_code FROM utilisateur WHERE mail = :email;");
-        $req->execute( array("email" => $email) );
+        $req = $bdd->prepare("SELECT mail, verification_code, role_user FROM utilisateur WHERE mail = :email;");
+        $req->bindValue(':email', $email, PDO::PARAM_STR);
+        $req->execute();
         $reponse = $req->fetch();
-        if($reponse) {
+        
+        if ($reponse) {
             $verification_code = $reponse['verification_code'];
-            if($connect == $verification_code) {
-                header('Location: ../connected/home.php');
+            if ($connect == $verification_code) {
+                $role = $reponse['role_user'];
+                if($role == 'utilisateur'){
+                    header('Location: ../connected/home.php');
+                }
+                else if($role == 'admin'){
+                    header('Location: ../../admin/home_backoffice.php');
+                }else{
+                    header('Location: confirmation_connexion.php?wrong_role=true');
+                }
+                
+                exit;
             } else {
                 header('Location: confirmation_connexion.php?wrong_code=true');
+                exit;
             }
+        } else {
+            header('Location: confirmation_connexion.php?no_user=true');
+            exit;
         }
     }
 }
