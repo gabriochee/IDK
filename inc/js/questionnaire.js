42862c958bb539d1fcee211d9f3ef492c4fc6212
gabriochee <gabypla2005@gmail.com>
2024-06-16 21:40:00 +0200

    Removed localhost !!!

--- inc/js/questionnaire.js
+++ inc/js/questionnaire.js
@@ -127,7 +127,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 <h2>Vos recommandations :</h2>
                 <h3>Veuillez patientez que les resultats se chargent</h3>
             `;                
-        fetch('http://localhost:8888/IDK/inc/php/function_questionnaire.php', {
+        fetch('../../inc/php/function_questionnaire.php', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(answers),
