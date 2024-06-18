4d23728a54cb38965754ba489787ec21e1957041
BelarbiLM <leomehdibelarbi@gmail.com>
2024-06-18 10:39:25 +0200

    maj

--- inc/php/function_edit_newsletter.php
+++ inc/php/function_edit_newsletter.php
@@ -190,24 +190,22 @@ AND id_bloc = 137;
             }
     
         } catch (Exception $e) {
-            echo "Erreur lors de l'envoi du message : " . $mail->ErrorInfo;
+            echo "<div class='alert alert-danger text-center' role='alert'>Erreur lors de l'envoi du message : " . $mail->ErrorInfo . "</div>";
         }
 
         try {
             // Enregistrer dans la base de données si l'envoi est réussi
             if ($mail->send()) {
-                $req2_news = $bdd->prepare("INSERT INTO contenu(page_appartenance, titre, corps, last_titre, last_corps) VALUES ('newsletter', :sujet, :corps, :last_titre, :last_corps)");
+                $req2_news = $bdd->prepare("INSERT INTO contenu(page_appartenance, titre, corps) VALUES ('Newsletter', :sujet, :corps)");
                 $req2_news->bindParam(':sujet', $_POST["subject"]);
                 $req2_news->bindParam(':corps', $_POST["corps_message"]);
-                $req2_news->bindParam(':last_titre', $_POST["subject"]);
-                $req2_news->bindParam(':last_corps', $_POST["corps_message"]);
                 $req2_news->execute();
-                echo "Le message a été envoyé avec succès";
+                echo '<div class="alert alert-success text-center" role="alert">Le message a été envoyé avec succès</div>';
             } else {
-                echo "Erreur lors de l'envoi du message : " . $mail->ErrorInfo;
+                echo "<div class='alert alert-danger text-center' role='alert'>Erreur lors de l'envoi du message : " . $mail->ErrorInfo . "</div>";
             }
         } catch (Exception $e) {
-            echo "Erreur lors de l'envoi du message : " . $mail->ErrorInfo;
+            echo "<div class='alert alert-danger text-center' role='alert'>Erreur lors de l'envoi du message : " . $mail->ErrorInfo . "</div>";
         }
     }
 ?>
