92ae0743e7e00cef53999dcda160dcf1a25751dc
gabriochee <gabypla2005@gmail.com>
2024-07-03 16:41:20 +0200

    You can now add a movie to your personnal lists directly on its page. (C'etait LONGGGGGGGGGG)

--- client/connected/oeuvre.php
+++ client/connected/oeuvre.php
@@ -147,10 +147,32 @@
                         <p class="m-0">Rédiger/Modifier ma critique</p>
                         <i class="bi bi-chat-left-dots ms-3"></i>
                     </div>
-                    <div class="col-3 d-flex justify-content-center align-items-center border border-2 border-end-0 border-dark color-custom-1 menu-oeuvre">
+                    <div class="col-3 d-flex justify-content-center align-items-center border border-2 border-end-0 border-dark color-custom-1 menu-oeuvre" data-bs-toggle="modal" data-bs-target="#addMovieToListModal">
                         <p class="m-0">Ajouter à une liste</p>
                         <i class="bi bi-plus-circle ms-3"></i>
                     </div>
+
+                    <div class="modal fade" id="addMovieToListModal" tabindex="-1" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5">Ajouter ce film à votre liste</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body d-flex flex-column justify-content-start">
+                                    <label for="search-list-input" class="form-label text-start">Rechercher</label>
+                                    <input type="search" class="form-control mb-2" id="search-list-input" onkeydown="searchMyLists(this, this.value)">
+                                    <div class="container d-flex flex-column form-check" id="lists-result-container">
+                                        
+                                    </div>
+                                </div>
+                                <div class="modal-footer d-flex justify-content-between">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <div class="col-1 d-flex justify-content-center align-items-center border border-2 border-dark color-custom-1 menu-oeuvre">
                         <i class="bi bi-share" data-bs-toggle="modal" data-bs-target="#shareMovieModal"></i>
 
@@ -300,9 +322,6 @@
                         <button class="w-100 btn btn-secondary btn-warning border-dark mt-2" type="submit" data-mdb-button-init data-mdb-ripple-init name="send_comment">Envoyer</button>
                     </form>
 
-
-
-
                 </div>
             </div>
         </div>
@@ -310,6 +329,7 @@
         <?php require_once('../../inc/components/connected/footer.php'); ?>
         <script src="../../inc/js/oeuvre.js"></script>
         <script src="../../inc/js/search_movie.js"></script>
+        <script src="../../inc/js/add_movie_to_my_lists.js"></script>
         <script>
             document.addEventListener('DOMContentLoaded', function() {
                 var privee = document.getElementById("private-comment");
@@ -373,19 +393,6 @@
                         });
                 });
             });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
             const cinq = document.getElementById("commentaire-cinq");
             const quatre = document.getElementById("commentaire-quatre");
--- inc/php/add_to_list.php
+++ inc/php/add_to_list.php
@@ -2,10 +2,22 @@
 require_once('db.php');
 
 if (isset($_GET['id_work']) && isset($_GET['id_liste'])){
-    $req = $bdd->prepare("INSERT INTO element_liste(id_liste, id_work, date_ajout) VALUES (:id_liste, :id_work, :date_ajout);");
-    $req->bindParam(":id_liste", $_GET['id_liste']);
-    $req->bindParam(":id_work", $_GET['id_work']);
-    $req->bindValue(":date_ajout", date("Y-m-d H:i:s"));
+    $id_work = $_GET['id_work'];
+    $id_liste = $_GET['id_liste'];
+    $test_req = $bdd->prepare("SELECT COUNT(id_liste) FROM element_liste WHERE id_liste = :id_liste AND id_work = :id_work;");
+    $test_req->bindParam(':id_work', $id_work);
+    $test_req->bindParam(':id_liste', $id_liste);
+    $test_req->execute();
 
-    $req->execute();
+    if (!$test_req->fetch()['COUNT(id_liste)']) {
+        $req = $bdd->prepare("INSERT INTO element_liste(id_liste, id_work, date_ajout) VALUES (:id_liste, :id_work, :date_ajout);");
+        $req->bindParam(":id_liste", $id_liste);
+        $req->bindParam(":id_work", $id_work);
+        $req->bindValue(":date_ajout", date("Y-m-d H:i:s"));
+
+        $req->execute();
+        echo json_encode([]);
+    } else {
+        echo json_encode(["error" => "Cet élément est déjà présent dans la liste."]);
+    }
 }
