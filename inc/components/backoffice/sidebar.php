121376738b5225f96b17ab52b3a5f12729b2086e
BelarbiLM <leomehdibelarbi@gmail.com>
2024-07-07 12:32:56 +0200

    maj

--- inc/components/backoffice/sidebar.php
+++ inc/components/backoffice/sidebar.php
@@ -1,4 +1,4 @@
-<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse overflow-auto no-overflow-y">
     <div class="position-sticky pt-3">
         <ul class="nav flex-column mt-5">
             <li class="nav-item">
